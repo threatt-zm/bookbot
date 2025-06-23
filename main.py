67e01af6a40006dea82a3fef4fedebd789015ce8
threatt-zm <10990825@live.mercer.edu>
2025-06-23 04:17:53 -0400

    added char count functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count
+from stats import get_word_count, get_char_count
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -10,5 +10,7 @@ def main():
     #print(book_text)
     num_words = get_word_count(book_text)
     print(f"{num_words} words found in the document")
+    num_chars = get_char_count(book_text)
+    print(num_chars)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_word_count(text):
     words = text.split()
     return len(words)
+
+def get_char_count(text):
+    characters = {}
+    for char in text:
+        char = char.lower()
+        if char not in characters:
+            characters[char] = 1
+        else:
+            characters[char] += 1
+    return characters
