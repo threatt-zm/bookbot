644c7d8829eda9bb47b48c9307706c64fbe201f4
threatt-zm <10990825@live.mercer.edu>
2025-06-23 16:23:58 -0400

    updated to allow input from system arguments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_word_count, get_char_count, sort_dict
 
 def get_book_text(filepath):
@@ -6,9 +7,13 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_text = get_book_text(sys.argv[1])
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    book_text = get_book_text("./books/frankenstein.txt")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     num_words = get_word_count(book_text)
     print(f"Found {num_words} total words")
