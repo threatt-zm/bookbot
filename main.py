7bc971bd5d6de1ede3cf95132e98c2430514a462
threatt-zm <10990825@live.mercer.edu>
2025-06-23 04:01:43 -0400

    put get_word_count() in stats file for code refactoring

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from stats import get_word_count
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
-
-def get_word_count(text):
-    words = text.split()
-    return len(words)
 
 def main():
     book_text = get_book_text("./books/frankenstein.txt")
